36eeb9c2521b8c7fdefcd13afe9f56c05da87438
Ido Kenan <107796750+ido-kenan@users.noreply.github.com>
2024-12-31 11:45:16 +0200

    Update ESM to Support Elasticsearch 7.17's Removal of Mapping Types
    This pull request updates the ESM tool to ensure compatibility with Elasticsearch 7.17, which has deprecated and removed mapping types.

--- domain.go
+++ domain.go
@@ -22,7 +22,6 @@ type Indexes map[string]interface{}
 
 type Document struct {
 	Index   string                 `json:"_index,omitempty"`
-	Type    string                 `json:"_type,omitempty"`
 	Id      string                 `json:"_id,omitempty"`
 	source  map[string]interface{} `json:"_source,omitempty"`
 	Routing string                 `json:"routing,omitempty"` //after 6, only `routing` was supported
